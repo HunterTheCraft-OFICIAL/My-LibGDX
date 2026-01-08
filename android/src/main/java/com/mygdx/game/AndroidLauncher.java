f1f90a191c0856e13c6687a0bea757708e16e380
HunterTheCraft-OFICIAL <impahunter67@gmail.com>
2026-01-08 13:29:21 -0300

    [fix]: My-LibGDX (libGDX Setup => Liftoff GDX)

--- android/src/main/java/com/mygdx/game/AndroidLauncher.java
+++ android/src/main/java/com/mygdx/game/AndroidLauncher.java
@@ -1,10 +1,10 @@
 package com.mygdx.game;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import gdx.liftoff.Main;
+// Alteração 1: Importar a classe correta do seu projeto Core
+import com.mygdx.game.MainClass; 
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
@@ -12,7 +12,9 @@ public class AndroidLauncher extends AndroidApplication {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
-        configuration.useImmersiveMode = true; // Recommended, but not required.
-        initialize(new Main(), configuration);
+        configuration.useImmersiveMode = true; 
+        
+        // Alteração 2: Trocar 'new Main()' por 'new MainClass()'
+        initialize(new MainClass(), configuration);
     }
 }
